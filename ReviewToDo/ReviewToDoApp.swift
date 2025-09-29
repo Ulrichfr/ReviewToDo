29198a019aa3dda7f1017271d266040fd272fedb
Ulrich <ulrich.jr@gmail.com>
2025-09-29 22:16:20 +0200

    Initial Commit

---
+++ ReviewToDo/ReviewToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewToDoApp.swift
+//  ReviewToDo
+//
+//  Created by Ulrich Rozier on 29/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReviewToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
